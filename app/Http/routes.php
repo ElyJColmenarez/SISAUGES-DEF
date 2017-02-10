c82c0b40fce5fa48f73bb63c03d9898e84f39780
Ely J. Colmenarez M <ElyJColmenarez@gmail.com>
2017-02-10 02:01:51 -0400

    Creados los grupos de rutas, aun faltan las llamadas a metodos

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,9 +13,6 @@
 |
 */
 
-/*Route::get('/', function () {
-    return view('auth.login');
-});*/
 
 
 Route::get('/', ['uses' => 'Auth\AuthController@index', 'as' => '/']);
@@ -23,4 +20,69 @@ Route::get('/', ['uses' => 'Auth\AuthController@index', 'as' => '/']);
 
 Route::auth();
 
-Route::get('/home', 'HomeController@index');
+Route::group(['prefix'=>'superuser', 'namespace'=>'SuperUser', 'middleware'=>'auth', 'as'=>'superuser'],function(){
+
+    Route::group(['prefix'=>'auditoria', 'namespace'=>'Auditoria', 'as'=>'auditoria'], function(){
+
+    });
+
+    Route::group(['prefix'=>'rol', 'namespace'=>'Rol', 'as'=>'rol'], function(){
+
+    });
+
+    Route::group(['prefix'=>'statustutor', 'namespace'=>'StatusTutor', 'as'=>'statustutor'], function(){
+
+    });
+
+});
+
+Route::group(['prefix'=>'admin', 'namespace'=>'Admin', 'middleware'=>'auth', 'as'=>'admin'], function(){
+
+    Route::group(['prefix'=>'usuario', 'namespace'=>'Usuario','as'=>'usuario'], function(){
+
+    });
+
+    Route::group(['prefix'=>'sectorproyecto', 'namespace'=>'SectorProyecto', 'as'=>'sectorproyecto'], function(){
+
+    });
+
+    Route::group(['prefix'=>'tecnicaestudio', 'namespace'=>'TecnicaEstudio', 'as'=>'tecnicaestudio'], function(){
+
+    });
+
+    Route::group(['prefix'=>'statustutor', 'namespace'=>'StatusTutor', 'as'=>'statustutor'], function(){
+
+    });
+
+
+});
+
+Route::group(['prefix'=>'operador', 'namespace'=>'Operador', 'middleware'=>'auth', 'as'=>'operador'],function(){
+
+    Route::group(['prefix'=>'tutor', 'namespace'=>'Tutor', 'as'=>'tutor'], function(){
+
+    });
+
+    Route::group(['prefix'=>'estudiante', 'namespace'=>'Estudiante', 'as'=>'estudiante'], function(){
+
+    });
+
+    Route::group(['prefix'=>'institucion', 'namespace'=>'Institucion', 'as'=>'institucion'], function(){
+
+    });
+
+
+    Route::group(['prefix'=>'departamento', 'namespace'=>'Departamento', 'as'=>'departamento'], function(){
+
+    });
+
+
+    Route::group(['prefix'=>'proyecto', 'namespace'=>'Proyecto', 'as'=>'proyecto'], function(){
+
+    });
+
+    Route::group(['prefix'=>'muestra', 'namespace'=>'Muestra','as'=>'muestra'], function(){
+
+    });
+
+});
