5efb712df2cd4aca09b340f7d0bb650d19fb026a
Ely J. Colmenarez M <ElyJColmenarez@gmail.com>
2017-02-28 19:54:09 -0400

    modulos de rafael agregados al sistema y archivos de migracfiones

--- app/Http/Controllers/DepartamentoController.php
+++ app/Http/Controllers/DepartamentoController.php
@@ -3,62 +3,157 @@
 namespace SISAUGES\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use SISAUGES\Models\Departamento;
 use SISAUGES\Http\Requests;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\View;
 
 class DepartamentoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+
+
+        $departamento = DB::table('departamento')->leftJoin('institucion', 'departamento.id_institucion', '=', 'institucion.id_institucion')
+            ->select('departamento.id_departamento', 'departamento.descripcion_departamento', 'institucion.id_institucion','institucion.nombre_institucion',  'departamento.status')
+            ->paginate(20);
+
+        return view('departamento.index',compact('departamento'));
+
+    }
+
+    public function renderform(Request $request){
+
+
+        if ($request->typeform=='add') {
+            $action="departamento/crear";
+        }elseif($request->typeform=='modify'){
+            $action="departamento/modificar/".$request->field_id;;
+        }elseif ($request->typeform=='deleted') {
+            $action="departamento/eliminar/".$request->field_id;
+        }
+
+        $departamento = Departamento::find($request->field_id);
+
+        if ($request->typeform=='deleted') {
+            $fields=false;
+        }else{
+
+            // $instituciones=Institucion::all()->toArray();
+
+            $instituciones=DB::table('institucion')->select(array('id_institucion','nombre_institucion'))->get();
+
+            $instituciones = json_decode(json_encode($instituciones), true);
+
+
+            $arrayI = array();
+            $arrayD = array();
+            for ($i=0; $i < sizeof($instituciones) ; $i++) {
+                array_push($arrayI,$instituciones[$i]["id_institucion"]);
+                array_push($arrayD,$instituciones[$i]["nombre_institucion"]);
+            }
+
+            $res = array_combine($arrayI, $arrayD);
+
+
+            $fields=array(
+                'field_id'=>array(
+                    'type'  => 'hidden',
+                    'value' => $request->field_id,
+                    'id'    => 'field_id'
+                ),
+
+                'descripcion_departamento' => array(
+                    'type'  => 'text',
+                    'value' => (empty($departamento))? '' : $departamento->descripcion_departamento,
+                    'id'    => 'descripcion_departamento',
+                    'label' => 'Nombre '
+                ),
+
+                'id_institucion' => array(
+                    'type'  => 'select',
+                    'value' => (empty($departamento))? '' : $departamento->id_institucion,
+                    'id'    => 'id_institucion',
+                    'label' => 'Institucion',
+                    'options'   => $res
+                ),
+
+                'status' => array(
+                    'type'      => 'select',
+                    'value'     => (empty($departamento))? '' : $departamento->status,
+                    'id'        => 'status',
+                    'validaciones'=>array(
+                        'obligatorio'
+                    ),
+                    'label'     => 'Status',
+                    'options'   => array(
+                        ''=>'Seleccione...',
+                        'true'=>'Activo',
+                        'false'=>'Inactivo'
+                    )
+                )
+            );
+
+
+        }
+
+        $htmlbody=View::make('layouts.regularform',compact('action','fields'))->render();
+
+
+        if ($htmlbody) {
+            $retorno=array(
+                'result'=>true,
+                'html'  =>$htmlbody
+            );
+        }else{
+            $retorno=array(
+                'result'=>false,
+            );
+        }
+
+        echo json_encode($retorno);
+
     }
 
     /**
-     * Show the form for creating a new resource.
+     * Metodo diseñado para direccionar a la pantalla de agregar un departamento
      *
-     * @return \Illuminate\Http\Response
+     * Este metodo redirige a la pantalla agregar departamento
+     * la cual mostrara un formulario con los campos necesarios para almacenar
+     * en la base de datos
+     *
+     * @param void
+     *
+     * @return $departamento devuelve objeto de tipo departamento
      */
-    public function create()
-    {
-        //
-    }
+    public function store(Request $request){
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+        $departamento=new Departamento($request->all());
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+        $aux=$request->all();
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
+        $cont=0;
+
+        foreach ($aux as $key => $value) {
+
+            $value=trim($value);
+
+            if ($value=='' &&  $key!='_token') {
+
+                //   $cont++;
+            }
+        }
+
+        if ($cont>0) {
+            $val=false;
+        }else{
+            $val=$departamento->save();
+
+        }
+
+        return $val;
+
     }
 
     /**
@@ -68,19 +163,124 @@ class DepartamentoController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
-    {
-        //
+
+    public function update($request, $id){
+
+        $departamento=Departamento::find($id);
+
+
+        $aux=$request->all();
+
+        $cont=0;
+
+        foreach ($aux as $key => $value) {
+
+            $value=trim($value);
+
+            if ($value=='' && $key!='_token') {
+                $cont++;
+            }
+        }
+
+        if ($cont>0) {
+            $val=false;
+        }else{
+
+            $departamento->descripcion_departamento=$request->descripcion_departamento;
+            $departamento->id_institucion=$request->id_institucion;
+            $departamento->status=$request->status;
+
+            $val=$departamento->save();
+        }
+
+        return $val;
+
     }
 
+
+
+
     /**
-     * Remove the specified resource from storage.
+     * Metodo diseñado para eliminar los datos de un departamento en la base de datos
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param $id codigo de asociación del departamento en la base de datos
+     *
+     * @return $menssage retorna el resultado de la operación.
      */
-    public function destroy($id)
-    {
-        //
+
+
+    public function destroy($id){
+        $departamento=Departamento::find($id);
+        $departamento->status='false';
+        $val=$departamento->save();
+        return $val;
+    }
+
+
+
+    public function ajaxRegularStore(Request $request){
+
+        $val=$this->store($request);
+
+        $retorno=array();
+
+        if ($val) {
+            //Datos Validos
+            $retorno['resultado']='success';
+            $retorno['mensaje']='El registro de los datos fue exitoso...';
+
+        }else{
+            //Datos Invalidos
+            $retorno['resultado']='danger';
+            $retorno['mensaje']='Los datos no suministrados no son validos';
+
+        }
+
+        echo json_encode($retorno);
+
+    }
+
+    public function ajaxRegularUpdate(Request $request, $id){
+
+        $val=$this->update($request,$id);
+
+        $retorno=array();
+
+        if ($val) {
+            //Datos Validos
+            $retorno['resultado']='success';
+            $retorno['mensaje']='Actualizacion de registro de los datos fue exitoso...';
+
+        }else{
+            //Datos Invalidos
+            $retorno['resultado']='danger';
+            $retorno['mensaje']='Los datos no suministrados no son validos';
+
+        }
+
+        echo json_encode($retorno);
+
+    }
+
+    public function ajaxRegularDestroy(Request $request,$id){
+
+        $val=$this->destroy($id);
+
+        $retorno=array();
+
+        if ($val) {
+            //Datos Validos
+            $retorno['resultado']='success';
+            $retorno['mensaje']='Desactivo fue exitoso...';
+
+        }else{
+            //Datos Invalidos
+            $retorno['resultado']='danger';
+            $retorno['mensaje']='Los datos no suministrados no son validos.';
+
+        }
+
+        echo json_encode($retorno);
+
     }
 }
--- app/Http/Controllers/TecnicaEstudioController.php
+++ app/Http/Controllers/TecnicaEstudioController.php
@@ -3,62 +3,133 @@
 namespace SISAUGES\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use SISAUGES\Models\TecnicaEstudio;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\View;
 use SISAUGES\Http\Requests;
 
 class TecnicaEstudioController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+
+
+        $tecnicas=TecnicaEstudio::orderBy('descripcion_tecnica_estudio', 'desc')->paginate(20);
+
+        return view('tecnicasEstudio.index',compact('tecnicas'));
+
+    }
+
+    public function renderform(Request $request){
+
+
+        if ($request->typeform=='add') {
+            $action="tecnicasEstudio/crear";
+        }elseif($request->typeform=='modify'){
+            $action="tecnicasEstudio/modificar/".$request->field_id;;
+        }elseif ($request->typeform=='deleted') {
+            $action="tecnicasEstudio/eliminar/".$request->field_id;
+        }
+
+        $tecnicasEstudio = TecnicaEstudio::find($request->field_id);
+
+        if ($request->typeform=='deleted') {
+            $fields=false;
+        }else{
+
+
+
+            $fields=array(
+                'field_id'=>array(
+                    'type'  => 'hidden',
+                    'value' => $request->field_id,
+                    'id'    => 'field_id'
+                ),
+
+                'descripcion_tecnica_estudio' => array(
+                    'type'  => 'text',
+                    'value' => (empty($tecnicasEstudio))? '' : $tecnicasEstudio->descripcion_tecnica_estudio,
+                    'id'    => 'descripcion_tecnica_estudio',
+                    'label' => 'Nombre '
+                ),
+
+
+                'status' => array(
+                    'type'      => 'select',
+                    'value'     => (empty($tecnicasEstudio))? '' : $tecnicasEstudio->status,
+                    'id'        => 'status',
+                    'validaciones'=>array(
+                        'obligatorio'
+                    ),
+                    'label'     => 'Status',
+                    'options'   => array(
+                        ''=>'Seleccione...',
+                        'true'=>'Activo',
+                        'false'=>'Inactivo'
+                    )
+                )
+            );
+
+
+        }
+
+        $htmlbody=View::make('layouts.regularform',compact('action','fields'))->render();
+
+
+        if ($htmlbody) {
+            $retorno=array(
+                'result'=>true,
+                'html'  =>$htmlbody
+            );
+        }else{
+            $retorno=array(
+                'result'=>false,
+            );
+        }
+
+        echo json_encode($retorno);
+
     }
 
     /**
-     * Show the form for creating a new resource.
+     * Metodo diseñado para direccionar a la pantalla de agregar un tecnicasEstudio
      *
-     * @return \Illuminate\Http\Response
+     * Este metodo redirige a la pantalla agregar tecnicasEstudio
+     * la cual mostrara un formulario con los campos necesarios para almacenar
+     * en la base de datos
+     *
+     * @param void
+     *
+     * @return $tecnicasEstudio devuelve objeto de tipo tecnicasEstudio
      */
-    public function create()
-    {
-        //
-    }
+    public function store(Request $request){
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+        $tecnicasEstudio=new TecnicaEstudio($request->all());
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+        $aux=$request->all();
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
+        $cont=0;
+
+        foreach ($aux as $key => $value) {
+
+            $value=trim($value);
+
+            if ($value=='' &&  $key!='_token') {
+
+                //   $cont++;
+            }
+        }
+
+        if ($cont>0) {
+            $val=false;
+        }else{
+            $val=$tecnicasEstudio->save();
+
+        }
+
+        return $val;
+
     }
 
     /**
@@ -68,19 +139,123 @@ class TecnicaEstudioController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
-    {
-        //
+
+    public function update($request, $id){
+
+        $tecnicasEstudio=TecnicaEstudio::find($id);
+
+
+        $aux=$request->all();
+
+        $cont=0;
+
+        foreach ($aux as $key => $value) {
+
+            $value=trim($value);
+
+            if ($value=='' && $key!='_token') {
+                $cont++;
+            }
+        }
+
+        if ($cont>0) {
+            $val=false;
+        }else{
+
+            $tecnicasEstudio->descripcion_tecnica_estudio=$request->descripcion_tecnica_estudio;
+            $tecnicasEstudio->status=$request->status;
+
+            $val=$tecnicasEstudio->save();
+        }
+
+        return $val;
+
     }
 
+
+
+
     /**
-     * Remove the specified resource from storage.
+     * Metodo diseñado para eliminar los datos de un tecnicasEstudio en la base de datos
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param $id codigo de asociación del tecnicasEstudio en la base de datos
+     *
+     * @return $menssage retorna el resultado de la operación.
      */
-    public function destroy($id)
-    {
-        //
+
+
+    public function destroy($id){
+        $tecnicasEstudio=TecnicaEstudio::find($id);
+        $tecnicasEstudio->status='false';
+        $val=$tecnicasEstudio->save();
+        return $val;
+    }
+
+
+
+    public function ajaxRegularStore(Request $request){
+
+        $val=$this->store($request);
+
+        $retorno=array();
+
+        if ($val) {
+            //Datos Validos
+            $retorno['resultado']='success';
+            $retorno['mensaje']='El registro de los datos fue exitoso...';
+
+        }else{
+            //Datos Invalidos
+            $retorno['resultado']='danger';
+            $retorno['mensaje']='Los datos no suministrados no son validos';
+
+        }
+
+        echo json_encode($retorno);
+
+    }
+
+    public function ajaxRegularUpdate(Request $request, $id){
+
+        $val=$this->update($request,$id);
+
+        $retorno=array();
+
+        if ($val) {
+            //Datos Validos
+            $retorno['resultado']='success';
+            $retorno['mensaje']='Actualizacion de registro de los datos fue exitoso...';
+
+        }else{
+            //Datos Invalidos
+            $retorno['resultado']='danger';
+            $retorno['mensaje']='Los datos no suministrados no son validos';
+
+        }
+
+        echo json_encode($retorno);
+
+    }
+
+    public function ajaxRegularDestroy(Request $request,$id){
+
+        $val=$this->destroy($id);
+
+        $retorno=array();
+
+        if ($val) {
+            //Datos Validos
+            $retorno['resultado']='success';
+            $retorno['mensaje']='Desactivo fue exitoso...';
+
+        }else{
+            //Datos Invalidos
+            $retorno['resultado']='danger';
+            $retorno['mensaje']='Los datos no suministrados no son validos.';
+
+        }
+
+        echo json_encode($retorno);
+
     }
 }
